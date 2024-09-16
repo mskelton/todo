b89dcfb6b433088bcfdee671da35391ab115834a
Mark Skelton <mdskelton99@gmail.com>
2024-09-16 08:44:38 -0500

    Fix sync

--- internal/models/task.go
+++ internal/models/task.go
@@ -12,32 +12,44 @@ type DueDate struct {
 	Timezone    string `json:"timezone"`
 }
 
+type DurationUnit string
+
+const (
+	Minute DurationUnit = "minute"
+	Day    DurationUnit = "day"
+)
+
+type Duration struct {
+	Amount int          `json:"amount"`
+	Unit   DurationUnit `json:"unit"`
+}
+
 type Task struct {
-	AddedAt        string                       `json:"added_at"`
-	AddedByUID     string                       `json:"added_by_uid"`
-	AssignedByUID  *string                      `json:"assigned_by_uid"`
-	Checked        bool                         `json:"checked"`
-	ChildOrder     int32                        `json:"child_order"`
-	Collapsed      bool                         `json:"collapsed"`
-	CompletedAt    *string                      `json:"completed_at"`
-	Content        string                       `json:"content"`
-	DayOrder       int                          `json:"day_order"`
-	Description    string                       `json:"description"`
-	Due            *datatypes.JSONType[DueDate] `json:"due"`
-	Duration       *string                      `json:"duration"`
-	ID             string                       `json:"id"`
-	IsDeleted      bool                         `json:"is_deleted"`
-	Labels         datatypes.JSONSlice[string]  `json:"labels"`
-	ParentID       *string                      `json:"parent_id"`
-	Priority       int                          `json:"priority"`
-	ProjectID      string                       `json:"project_id"`
-	ResponsibleUID *string                      `json:"responsible_uid"`
-	SectionID      *string                      `json:"section_id"`
-	SyncID         *string                      `json:"sync_id"`
-	UpdatedAt      string                       `json:"updated_at"`
-	UserID         string                       `json:"user_id"`
-	V2ID           string                       `json:"v2_id"`
-	V2ParentID     *string                      `json:"v2_parent_id"`
-	V2ProjectID    string                       `json:"v2_project_id"`
-	V2SectionID    *string                      `json:"v2_section_id"`
+	AddedAt        string                        `json:"added_at"`
+	AddedByUID     string                        `json:"added_by_uid"`
+	AssignedByUID  *string                       `json:"assigned_by_uid"`
+	Checked        bool                          `json:"checked"`
+	ChildOrder     int32                         `json:"child_order"`
+	Collapsed      bool                          `json:"collapsed"`
+	CompletedAt    *string                       `json:"completed_at"`
+	Content        string                        `json:"content"`
+	DayOrder       int                           `json:"day_order"`
+	Description    string                        `json:"description"`
+	Due            *datatypes.JSONType[DueDate]  `json:"due"`
+	Duration       *datatypes.JSONType[Duration] `json:"duration"`
+	ID             string                        `json:"id"`
+	IsDeleted      bool                          `json:"is_deleted"`
+	Labels         datatypes.JSONSlice[string]   `json:"labels"`
+	ParentID       *string                       `json:"parent_id"`
+	Priority       int                           `json:"priority"`
+	ProjectID      string                        `json:"project_id"`
+	ResponsibleUID *string                       `json:"responsible_uid"`
+	SectionID      *string                       `json:"section_id"`
+	SyncID         *string                       `json:"sync_id"`
+	UpdatedAt      string                        `json:"updated_at"`
+	UserID         string                        `json:"user_id"`
+	V2ID           string                        `json:"v2_id"`
+	V2ParentID     *string                       `json:"v2_parent_id"`
+	V2ProjectID    string                        `json:"v2_project_id"`
+	V2SectionID    *string                       `json:"v2_section_id"`
 }
